de080ca256a9516d97334eb34bec3ce55675b5aa
Veryan4 <v.goodship@gmail.com>
2019-11-07 23:56:04 -0500

    typo

--- src/test/java/monitor/MonitorControllerTest.java
+++ src/test/java/monitor/MonitorControllerTest.java
@@ -49,7 +49,7 @@ class MonitorControllerTest {
 
 		@Test
     public void submit() throws Exception {
-        ResultActions resultActions = mockMvc.perform( MockMvcRequestBuilders.post("/")
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/")
                 .content(asJsonString(new Monitor(new ArrayList<>(), 1000, "https://google.com", "Monitor is Started")))
       					.contentType(MediaType.APPLICATION_JSON)
       					.accept(MediaType.APPLICATION_JSON))
