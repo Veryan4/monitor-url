5081a6d709671cdd00e4705d9ddde0fd34aae9c4
Veryan4 <v.goodship@gmail.com>
2019-11-08 11:15:20 -0500

    added streaming

--- src/main/java/monitor/MonitorController.java
+++ src/main/java/monitor/MonitorController.java
@@ -5,19 +5,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import monitor.models.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Controller
+package monitor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import monitor.models.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller
 public class MonitorController implements Runnable {
 
-    private Monitor monitor = new Monitor(new ArrayList<>(), 1000,  "https://google.com", "");
+    private Monitor monitor = new Monitor(new ArrayList < > (), 1000, "https://google.com", "");
     private Thread worker = new Thread(this);
     private final AtomicBoolean running = new AtomicBoolean(false);
+    private SseEmitter emitter = new SseEmitter();
+    private int count;
 
     @GetMapping("/")
     public String main(Model model) {
@@ -28,8 +55,14 @@ public class MonitorController implements Runnable {
         return "monitor"; //view in monitor.html
     }
 
+    @GetMapping("/stream")
+    public SseEmitter handleSse() {
+        this.emitter = new SseEmitter();
+        return this.emitter;
+    }
+
     @PostMapping("/")
-    public String monitorSubmit(@ModelAttribute Monitor newMonitor) {
+    public String start(@ModelAttribute Monitor newMonitor) {
         int newInterval = newMonitor.interval;
         if (Monitor.isIntervalValid(newInterval)) {
             this.monitor.interval = newInterval;
@@ -47,10 +80,9 @@ public class MonitorController implements Runnable {
             return "monitor"; //view in monitor.html
         }
         this.monitor.message = "Monitor is Running";
-        //@ModelAttribute seems to stop me from passing properties of this.monitor to newMonitor in bulk
         newMonitor.message = "Monitor is Running";
         newMonitor.statuses = this.monitor.statuses;
-        if (!this.worker.isAlive()) {
+        if (!this.worker.isAlive() || this.worker.isInterrupted()) {
             this.worker.start();
         }
         return "monitor"; //view in monitor.html
@@ -77,6 +109,7 @@ public class MonitorController implements Runnable {
                 System.out.println("Thread was interrupted, Failed to complete operation");
             }
             getStatus();
+            getStream();
         }
     }
 
@@ -101,6 +134,23 @@ public class MonitorController implements Runnable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void getStream() {
+        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+        sseMvcExecutor.execute(() - > {
+            try {
+                List < Status > statuses = this.monitor.getStatuses();
+                SseEventBuilder event = SseEmitter.event()
+                    .data(statuses.get(statuses.size() - 1))
+                    .id(String.valueOf(this.count))
+                    .name("message");
+                this.emitter.send(event);
+                this.count++;
+            } catch (Exception ex) {
+                this.emitter.completeWithError(ex);
+            }
+        });
     }
 
 }
