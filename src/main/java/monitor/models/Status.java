6b0f0b9fe8d4e6fd4227c85bf1f1dc7c69e8008b
Veryan4 <v.goodship@gmail.com>
2019-11-09 02:06:27 -0500

    clean up

--- src/main/java/monitor/models/Status.java
+++ src/main/java/monitor/models/Status.java
@@ -1,3 +1,5 @@
+package monitor;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -6,15 +8,25 @@ public class Status implements Serializable {
 
     private String time;
     private String status;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     public Status(Date time, String status) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         this.time = dateFormat.format(time);
         this.status = status;
     }
 
     public String getTime() {
         return time;
+    }
+
+    public Status setTime(Date time) {
+        this.time = dateFormat.format(time);
+        return this;
+    }
+
+    public Status setTime(String time) {
+        this.time = time;
+        return this;
     }
 
     public String getStatus() {
